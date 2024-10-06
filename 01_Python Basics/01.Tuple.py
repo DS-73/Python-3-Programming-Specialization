4572896774163d53042b52bdcf08386208229a4b
DS-73 <dhruvsaini73@gmail.com>
2024-10-06 12:28:39 +0530

    Add tuple containing one element

---
+++ 01_Python Basics/01.Tuple.py
@@ -0,0 +1,12 @@
+
+def display(ele):
+    print(f' >> {ele}, {type(ele)}')
+
+# This is an integer
+my_integer = (1)
+
+# Tuple need to end with , if only one lement is there
+my_tuple = (1,)
+
+display(my_integer)
+display(my_tuple)
